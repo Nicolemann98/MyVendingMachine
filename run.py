c82fcbb5fcb2df58ff115c40b2a263be3db08129
Nicolemann98 <>
2024-01-20 13:48:46 +0000

    Data set up into Product objects

--- run.py
+++ run.py
@@ -12,22 +12,64 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("my_vending_machine")
 
-product = SHEET.worksheet("product")
+product_worksheet = SHEET.worksheet("product")
 
-product_data = product.get_all_values()
+all_products = []
+
+class Product():
+    """
+    Creates instance of Product
+    """
+    def __init__(self, item_name, quantity, price):
+        self.item_name = item_name
+        self.quantity = quantity
+        self.price = price
+
+    def get_product_text(self):
+        """
+        Takes the product object and shows in a user readable format for the menu screen
+        """
+        return f"{self.item_name} - {self.format_price()}"
+
+    def format_price(self):
+        """
+        Takes the price from a number in pence (e.g. 124) and returns it in standard price format (e.g. £1.24)
+        """
+        length = len(self.price)
+        pounds = self.price[:length-2]
+        if (pounds == ""):
+            pounds = "0"
+        pence = self.price[length-2:]
+        return f"£{pounds}.{pence}"
+
+
+def set_up_products():
+    """
+    Takes the data from the product spreadsheet, creates a Product object for each row and adds to the all_products list
+    """
+    for product_unformatted in product_worksheet.get_all_values():
+        product = Product(product_unformatted[0], product_unformatted[1], product_unformatted[2])
+        all_products.append(product)
 
 def get_user_input():
+    """
+    Displays the welcome screen and asks the user for their item
+    """
     print("Welcome to the Vending Machine, what would you like today?")
     # The items and stock manager login will go here
-    print("--------------------------------------------------")
+    for i in range(1, len(all_products)):
+        product = all_products[i]
+        print(f"{i}: {product.get_product_text()}")
     return True
 
 
 def main():
     """
-    Run the vending machine workflow
+    Run the vending machine workflow from power-up to shut-down
     """
     print("Starting Vending Machine. Welcome!")
+    set_up_products()
+
     while True:
         should_quit = get_user_input()
         if (should_quit):
